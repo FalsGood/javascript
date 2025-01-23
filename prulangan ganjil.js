ab4c55d50c699fb8e8615927f4720abb720559d3
FalsGood <siagiannaufal101@gmail.com>
2025-01-23 09:48:49 +0700

    Delete prulangan ganjil.js

--- prulangan ganjil.js
+++
@@ -1,9 +0,0 @@
-let jumlah = 0;
-
-for (let i = 1; i <= 100; i++) {
-    if (i % 2 !== 0) {
-        jumlah += i;
-    }
-}
-
-console.log("Jumlah bilangan ganjil dari 1 hingga 100 adalah:", jumlah);
